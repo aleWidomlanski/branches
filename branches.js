b3bbfe67b5a8449f93f168166cacd532dfc97771
Alejandro Widomlanski <widomlanski@hotmail.com>
2020-11-05 20:14:13 -0300

    prueba cambiando de rama

---
+++ branches.js
@@ -0,0 +1,11 @@
+/* Crear una rama 
+git branch {branch}
+
+Cambiar de rama
+git checkout {branch}
+
+Traer actualizaciones de una rama 
+git pull origin {branch}
+
+Enviar cambios a la rama remota 
+git push origin {branch} */
